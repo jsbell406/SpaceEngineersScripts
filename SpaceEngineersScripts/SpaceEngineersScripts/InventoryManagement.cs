4294c07ac42d7a99092634c021d108f0f980e1e2
James Bell <masterpiece0310@hotmail.com>
2019-02-16 13:41:08 -0700

    Count Inventory

---
+++ SpaceEngineersScripts/SpaceEngineersScripts/InventoryManagement.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI.Ingame;
+
+namespace SpaceEngineersScripts
+{
+    
+    public class InventoryManagement
+    {
+        IMyGridTerminalSystem GridTerminalSystem = null;
+
+        
+        
+        public void Main()
+        {
+
+            Dictionary<string, long> inventoryCounts = GetCountOfInventoryItems();
+
+            foreach(KeyValuePair<string, long> keyValuePair in inventoryCounts)
+            {
+                //Echo(keyValuePair.Key + " " + keyValuePair.Value.ToString());
+            }
+
+
+        }
+
+        public Dictionary<string,long> GetCountOfInventoryItems()
+        {
+            Dictionary<string, long> inventoryCounts = new Dictionary<string, long>();
+            List<IMyInventoryItem> items = GetGridInventoryItems();
+            
+
+            foreach (IMyInventoryItem item in items)
+            {
+
+                long value = 0;
+                if (inventoryCounts.TryGetValue(GetInventoryItemByName(item), out  value))
+                {
+                    inventoryCounts[GetInventoryItemByName(item)] += item.Amount.RawValue;
+                }
+                else
+                {
+                    inventoryCounts.Add(GetInventoryItemByName(item), item.Amount.RawValue);
+                }   
+            }
+            return inventoryCounts;
+        }
+
+        public string GetInventoryItemByName(IMyInventoryItem item)
+        {
+            string itemName = item.GetDefinitionId().ToString();
+
+            char[] delimChar = "/".ToCharArray();
+
+            string[] newString = itemName.Split(delimChar);
+
+            return newString[1];
+        }
+
+        public List<IMyCargoContainer> GetAllCargoContainer()
+        {
+            List<IMyCargoContainer> myCargoContainers = new List<IMyCargoContainer>();
+
+            GridTerminalSystem.GetBlocksOfType(myCargoContainers);
+
+            return myCargoContainers;
+        }
+
+        public List<IMyInventory> GetMyInventory()
+        {
+            List<IMyCargoContainer> myCargoContainers = GetAllCargoContainer();
+
+            List<IMyInventory> inventory = new List<IMyInventory>();
+
+            foreach(IMyCargoContainer my in myCargoContainers)
+            {
+                inventory.Add(my.GetInventory());
+            }
+
+            return inventory;
+
+        }
+
+        public List<IMyInventoryItem> GetGridInventoryItems()
+        {
+            List<IMyInventoryItem> items = new List<IMyInventoryItem>();
+            List<IMyInventory> inventory = GetMyInventory();
+
+            foreach(IMyInventory inv in inventory)
+            {
+                items.AddRange(inv.GetItems());
+            }
+
+
+            return items;
+        }
+
+    }
+}
