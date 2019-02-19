d84bd4698875d999408cb118c81a000e40804f6a
James Bell <masterpiece0310@hotmail.com>
2019-02-18 23:18:47 -0700

    inventory counter prototype complete, working on assembler and refinery management, item management

--- SpaceEngineersScripts/SpaceEngineersScripts/InventoryManagement.cs
+++ SpaceEngineersScripts/SpaceEngineersScripts/InventoryManagement.cs
@@ -15,25 +15,87 @@ namespace SpaceEngineersScripts
     public class InventoryManagement
     {
         IMyGridTerminalSystem GridTerminalSystem = null;
+        List<IMyTerminalBlock> Blocks;
 
-        
-        
+
         public void Main()
         {
-            
-            Dictionary<string, IMyInventoryItem> inventory = GetInvetoryObjects();
 
-            foreach(KeyValuePair<string, IMyInventoryItem> keyValuePair in inventory)
+            Blocks = new List<IMyTerminalBlock>();
+            List<IMyAssembler> assemblers = new List<IMyAssembler>();
+            List<IMyRefinery> refineries = new List<IMyRefinery>();
+            List<IMyRadioAntenna> antennas = new List<IMyRadioAntenna>();
+            List<IMyTextPanel> textPanels = new List<IMyTextPanel>();
+            List<IMyCargoContainer> cargoContainers = new List<IMyCargoContainer>();
+
+           
+            GridTerminalSystem.GetBlocks(Blocks);
+            GridTerminalSystem.GetBlocksOfType(assemblers);
+            GridTerminalSystem.GetBlocksOfType(refineries);
+            GridTerminalSystem.GetBlocksOfType(antennas);
+            GridTerminalSystem.GetBlocksOfType(textPanels);
+            GridTerminalSystem.GetBlocksOfType(cargoContainers);
+
+            foreach (IMyAssembler block in assemblers)
             {
-                Echo(keyValuePair.Key + " " + keyValuePair.Value.Amount.ToString());
+                
+                Echo(block.CustomName);
             }
+
+            foreach (IMyRefinery block in refineries)
+            {
+
+                Echo(block.CustomName);
+            }
+            foreach (IMyRadioAntenna block in antennas)
+            {
+
+                Echo(block.CustomName);
+            }
+            foreach (IMyTextPanel block in textPanels)
+            {
+
+                Echo(block.CustomName);
+            }
+            foreach (IMyCargoContainer block in cargoContainers)
+            {
+
+                Echo(block.CustomName);
+            }
+            //Dictionary<string, IMyInventoryItem> InventoryObjects = GetInvetoryObjects();
+
+            //IMyTerminalBlock programmableBlock = Blocks.Where(b => b.CustomName == "Programmable block").FirstOrDefault();
+            //programmableBlock.CustomData = "";
+
+            //foreach (KeyValuePair<string, IMyInventoryItem> nameObjectPair in InventoryObjects)
+            //{
+            //    programmableBlock.CustomData += string.Format("{0}-{1}\n", nameObjectPair.Key, nameObjectPair.Value.Amount.ToString());
+            //    Echo(nameObjectPair.Key + " " + nameObjectPair.Value.Amount.ToString());
+            //}
+        }
+
+        public List<IMyInventory> GetInventories()
+        {
+            List<IMyInventory> inventory = new List<IMyInventory>();
+            List<IMyTerminalBlock> blocksWIthInventory = GetBlocksWithInventory();
+
+            foreach(IMyTerminalBlock block in blocksWIthInventory)
+            {
+                for(int x = 0; x < block.InventoryCount; x++)
+                {
+                    inventory.Add(block.GetInventory(x));
+                }
+            }
+        
+            return inventory;
+
         }
 
         public Dictionary<string, IMyInventoryItem> GetInvetoryObjects()
         {
             Dictionary<string, IMyInventoryItem> inventory = new Dictionary<string, IMyInventoryItem>();
-            List<IMyInventoryItem> items = GetGridInventoryItems();
-            
+            List<IMyInventoryItem> items = GetInventoryItems();
+
             foreach (IMyInventoryItem item in items)
             {
                 IMyInventoryItem invItem;
@@ -50,6 +112,30 @@ namespace SpaceEngineersScripts
             return inventory;
         }
 
+        public List<IMyInventoryItem> GetInventoryItems()
+        {
+            List<IMyInventoryItem> items = new List<IMyInventoryItem>();
+            List<IMyInventory> inventory = GetInventories();
+
+            foreach (IMyInventory inv in inventory)
+            {
+                items.AddRange(inv.GetItems());
+            }
+
+
+            return items;
+        }
+
+        public List<IMyTerminalBlock> GetBlocksWithInventory()
+        {
+            List<IMyTerminalBlock> BlocksWithInventory = new List<IMyTerminalBlock>();
+
+            BlocksWithInventory.AddRange(Blocks.Where(b => b.HasInventory));
+
+            return BlocksWithInventory;
+        }
+
+        
 
         public string GetInventoryItemByName(IMyInventoryItem item)
         {
@@ -60,41 +146,6 @@ namespace SpaceEngineersScripts
             string[] newString = itemName.Split(delimChar);
 
             return newString[1];
-        }
-
-        public List<IMyCargoContainer> GetAllCargoContainer()
-        {
-            List<IMyCargoContainer> myCargoContainers = new List<IMyCargoContainer>();
-
-            GridTerminalSystem.GetBlocksOfType(myCargoContainers);
-
-            return myCargoContainers;
-        }
-
-        public List<IMyInventory> GetMyInventory()
-        {
-            List<IMyCargoContainer> myCargoContainers = GetAllCargoContainer();
-
-            List<IMyInventory> inventory = new List<IMyInventory>();
-
-            inventory.AddRange(myCargoContainers.Select(c => c.GetInventory()));
-          
-            return inventory;
-
-        }
-
-        public List<IMyInventoryItem> GetGridInventoryItems()
-        {
-            List<IMyInventoryItem> items = new List<IMyInventoryItem>();
-            List<IMyInventory> inventory = GetMyInventory();
-
-            foreach(IMyInventory inv in inventory)
-            {
-                items.AddRange(inv.GetItems());
-            }
-
-
-            return items;
-        }
+        }  
     }
 }
